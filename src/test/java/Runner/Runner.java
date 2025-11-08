b5e330c737a6fea870a51d63e162daf35a49cec0
Shruti199521 <patilshruti259@gmail.com>
2025-11-08 22:05:23 +0530

    update the runner class

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -2,21 +2,20 @@ package Runner;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 
-public class Runner
-{
-	@RunWith(Cucumber.class)
-	@CucumberOptions(
-	    features = "src/test/resources/features",
-	    glue = {"StepDefinitions"},
-	    		 plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-html-report"},
-	    monochrome = true
-	)
-	public class TestRunner extends AbstractTestNGCucumberTests 
-	{
-		
-	}
-
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/Features",
+    glue = {"StepDefinitions"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-html-report.html",
+        "json:target/cucumber-reports/cucumber.json",
+        "junit:target/cucumber-reports/cucumber.xml"
+    },
+    monochrome = true,
+    publish = true
+)
+public class Runner {
 }
