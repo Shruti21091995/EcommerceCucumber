fff51a42d2163638c8b26b6f401010ae338ba03c
Shruti199521 <patilshruti259@gmail.com>
2025-11-08 21:43:33 +0530

    updated

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -1,10 +1,13 @@
 package Runner;
 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 public class Runner
 {
+	@RunWith(Cucumber.class)
 	@CucumberOptions(
 	    features = "src/test/resources/features",
 	    glue = {"StepDefinitions"},
